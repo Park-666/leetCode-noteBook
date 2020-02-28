bf446a0acf0639f5afcdd0a8e94145bcd2bcd620
TomPlayBoy <31658271+TomPlayBoy@users.noreply.github.com>
2020-02-28 22:14:12 +0800

    Create 160-相交链表.cpp

---
+++ 简单/160-相交链表.cpp
@@ -0,0 +1,26 @@
+/*暴力法，逐个比较节点地址是否相等*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *cruA = headA;
+        while (cruA) {
+            ListNode *cruB = headB;
+            while (cruB) {
+                if (cruA == cruB) {
+                    return cruA;
+                }
+                cruB = cruB->next;
+            }
+            cruA = cruA->next;
+        }
+        return NULL;
+    }
+};
