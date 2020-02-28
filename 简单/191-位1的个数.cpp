72e7db4edfa21844e5a16b09b406e8706c78806d
TomPlayBoy <31658271+TomPlayBoy@users.noreply.github.com>
2020-02-28 15:00:41 +0800

    Update 191-位1的个数.cpp

--- 简单/191-位1的个数.cpp
+++ 简单/191-位1的个数.cpp
@@ -13,3 +13,28 @@ public:
         return count;
     }
 };
+
+/*直接计算1的个数，利用 n*(n -1)*/
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while(n != 0) {
+            count++;
+            n &= (n - 1);
+        }
+        return count;
+    }
+};
+
+/*SWAR算法计算汉明重量*/
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        n = (n & 0x55555555) + ((n >> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+        n = (n & 0x0F0F0F0F) + ((n >> 4) & 0x0F0F0F0F);
+        n = (n * (0x01010101) >> 24);
+        return n;  
+    }
+};
