004d8fae81d864e8a0fbf049a1ad8099c3716769
TomPlayBoy <31658271+TomPlayBoy@users.noreply.github.com>
2020-02-29 21:04:35 +0800

    Update 160-相交链表.cpp

--- 简单/160-相交链表.cpp
+++ 简单/160-相交链表.cpp
@@ -25,3 +25,33 @@ public:
         return NULL;
     }
 };
+
+
+/*哈希表法，利用c++的stl::unordered_set。先对A链表构建哈希表，再遍历B链表*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        /*对A链表构建哈希表*/
+        std::unordered_set<ListNode*> set;
+        while (headA) {
+            set.insert(headA);
+            headA = headA->next;
+        }
+        /*遍历B链表，查找地址相同节点*/
+        while (headB) {
+            if (set.find(headB) != set.end()) {
+                return headB;
+            }
+            headB = headB->next;
+        }
+        return NULL;
+    }
+};
