de2b90534c4cdab0adc9816cfce2a372e9c2146f
TomPlayBoy <31658271+TomPlayBoy@users.noreply.github.com>
2020-03-02 20:36:14 +0800

    Update 27-移除元素.cpp

--- 简单/27-移除元素.cpp
+++ 简单/27-移除元素.cpp
@@ -15,3 +15,20 @@ public:
         return n;
     }
 };
+
+/*方法2，双指针法，覆盖法*/
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int i = 0;
+        int j = 0;
+        for (i = 0; i < n; i++) {
+            if (nums.at(i) != val) {
+                nums.at(j) = nums.at(i);
+                j++;
+            } 
+        }
+        return j;
+    }
+};
